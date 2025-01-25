d79fa528810e010e86faf6b5e542c0ecead8c54d
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2025-01-25 17:06:02 +0900

    폰트 변경

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Router from './Router.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import './main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
