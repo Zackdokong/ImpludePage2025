be837323a91aafdf100e9bcf2cafd44e579bfab7
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2025-01-28 00:14:11 +0900

    화살표 애니메이션 만듬

--- src/Main/mainpage.jsx
+++ src/Main/mainpage.jsx
@@ -1,24 +1,24 @@
-import './mainpage.css';
-import ImpludeLogo from '../assets/implude.svg';
-import styled, { keyframes } from 'styled-components';
-import ArrowBottom from '../assets/arrow_bottom.svg';
-import { useRef } from 'react';
-import Header from '../components/header'
-import Footer from '../components/footer'
+import "./mainpage.css";
+import ImpludeLogo from "../assets/implude.svg";
+import styled, { keyframes } from "styled-components";
+import ArrowBottom from "../assets/arrow_bottom.svg";
+import { useRef } from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 function MainPage() {
-    const phraseRef = useRef(null); // ImpludePhrase의 h1을 참조할 ref
+  const phraseRef = useRef(null); // ImpludePhrase의 h1을 참조할 ref
 
-    // ArrowBottom 클릭 시 실행되는 함수
-    const scrollToPhrase = () => {
-        phraseRef.current.scrollIntoView({
-            behavior: 'smooth', // 부드럽게 스크롤
-            block: 'center', // 요소의 시작 부분이 화면 상단에 맞춰짐
-        });
-    };
+  // ArrowBottom 클릭 시 실행되는 함수
+  const scrollToPhrase = () => {
+    phraseRef.current.scrollIntoView({
+      behavior: "smooth", // 부드럽게 스크롤
+      block: "center", // 요소의 시작 부분이 화면 상단에 맞춰짐
+    });
+  };
 
-    function messageBar(n) {
-        const animation = keyframes`
+  function messageBar(n) {
+    const animation = keyframes`
           0% {
             width: ${`${n / 24}rem`}
           }
@@ -28,41 +28,52 @@ function MainPage() {
           100% {
             width: ${`${n / 24}rem`}
           }
-        `
-        const ReturnSpan = styled.span`
-          width: ${`${n / 24}rem`};
-          height: ${'0.7rem'};
-          background: black;
-          display: inline-block;
-          position: relative;
-          bottom: 1.5vw;
-          animation: ${animation} 3s ease-in-out infinite;
-        `
-        return <ReturnSpan />
-    }
+        `;
+    const ReturnSpan = styled.span`
+      width: ${`${n / 24}rem`};
+      height: ${"0.7rem"};
+      background: black;
+      display: inline-block;
+      position: relative;
+      bottom: 1.5vw;
+      animation: ${animation} 3s ease-in-out infinite;
+    `;
+    return <ReturnSpan />;
+  }
 
-    return (
-        <>
-            <Header />
-            <div className="wrapper">
-                <div className="LogoWrapper">
-                    <img className="ImpludeLogo" src={ImpludeLogo} alt="Implude Logo" />
-                    <h1>#IMPLUDE</h1>
-                    <img className="ArrowBottom" src={ArrowBottom} alt="arrow" onClick={scrollToPhrase} /> {/* 클릭 시 scrollToPhrase 함수 호출 */}
-                </div>
-                <div className="ImpludePhrase" ref={phraseRef}>
-                    <h1>당신의 상상이 현실이 되도록</h1>
-                    <div>
-                        YOUR {messageBar(244)} <span className="PhraseBlue">IM</span>PACT <br />
-                        <span className="PhraseBlue">P</span>ASSION {messageBar(96)} <span className="PhraseBlue">L</span>EARN {messageBar(132)} <br />
-                        FUT<span className="PhraseBlue">U</span>RE {messageBar(188)} <span className="PhraseBlue">D</span>REAM <br />
-                        {messageBar(114)} POTENTIAL {messageBar(62)} T<span className="PhraseBlue">E</span>AM
-                    </div>
-                </div>
-            </div>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div className="wrapper">
+        <div className="LogoWrapper">
+          <img className="ImpludeLogo" src={ImpludeLogo} alt="Implude Logo" />
+          <h1>#IMPLUDE</h1>
+          <div>
+            <img
+              className="ArrowBottom animate"
+              src={ArrowBottom}
+              alt="arrow"
+              onClick={scrollToPhrase}
+            />
+          </div>
+        </div>
+        <div className="ImpludePhrase" ref={phraseRef}>
+          <h1>당신의 상상이 현실이 되도록</h1>
+          <div>
+            YOUR {messageBar(244)} <span className="PhraseBlue">IM</span>PACT{" "}
+            <br />
+            <span className="PhraseBlue">P</span>ASSION {messageBar(96)}{" "}
+            <span className="PhraseBlue">L</span>EARN {messageBar(132)} <br />
+            FUT<span className="PhraseBlue">U</span>RE {messageBar(188)}{" "}
+            <span className="PhraseBlue">D</span>REAM <br />
+            {messageBar(114)} POTENTIAL {messageBar(62)} T
+            <span className="PhraseBlue">E</span>AM
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default MainPage;
