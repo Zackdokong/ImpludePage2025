6c9b2463b5b9a8dd4c723b5a33d413b2c5388174
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2025-02-10 21:22:37 +0900

    ㅗ

--- src/Main/mainpage.jsx
+++ src/Main/mainpage.jsx
@@ -1,13 +1,18 @@
 import { useRef, useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Link, useNavigate } from "react-router-dom";
-import ImpludeLogo from "../assets/implude.svg";
-import ArrowBottom from "../assets/arrow_bottom.svg";
+import { useMediaQuery } from "react-responsive";
 import styled, { css, keyframes } from "styled-components";
+
+// Components
 import Header from "../components/header";
 import Footer from "../components/footer";
-import "./mainpage.css";
-import { useMediaQuery } from "react-responsive";
+
+// Assets
+import ImpludeLogo from "../assets/implude.svg";
+import ArrowBottom from "../assets/arrow_bottom.svg";
+
+// Project Thumbnails
 import YAMMY from "../assets/Project_Thumbnail/YAMMY.jpg";
 import VOAH from "../assets/Project_Thumbnail/VOAH.jpg";
 import MOLA from "../assets/Project_Thumbnail/MOLA.jpg";
@@ -18,15 +23,21 @@ import ITNun from "../assets/Project_Thumbnail/ITNun.jpg";
 import DINEON from "../assets/Project_Thumbnail/DINEON.jpg";
 import DAYCUBE from "../assets/Project_Thumbnail/DAYCUBE.jpg";
 import ASD from "../assets/Project_Thumbnail/ASD.jpg";
-import award1 from "../assets/award/1.jpg"
-import award2 from "../assets/award/2.jpg"
-import award3 from "../assets/award/3.jpg"
-import award4 from "../assets/award/4.jpg"
-import award5 from "../assets/award/5.jpg"
-import award6 from "../assets/award/6.jpg"
-import award7 from "../assets/award/7.jpg"
-import award8 from "../assets/award/8.jpg"
-import award9 from "../assets/award/9.jpg"
+
+// Awards
+import award1 from "../assets/award/1.jpg";
+import award2 from "../assets/award/2.jpg";
+import award3 from "../assets/award/3.jpg";
+import award4 from "../assets/award/4.jpg";
+import award5 from "../assets/award/5.jpg";
+import award6 from "../assets/award/6.jpg";
+import award7 from "../assets/award/7.jpg";
+import award8 from "../assets/award/8.jpg";
+import award9 from "../assets/award/9.jpg";
+
+// Styles
+import "./mainpage.css";
+
 
 
 const getAnimation = (n, isTabletOrMobile) => keyframes`
