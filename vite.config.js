6f6b7b46f2cb3a8aa2ecaeb2f20d19ad39c4ec1c
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2025-02-10 22:02:42 +0900

    최적화 시도

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsInlineLimit: 0, // 모든 파일을 별도 파일로 저장
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]', // 해시 포함된 파일 이름
+      },
+    },
+  },
 })
