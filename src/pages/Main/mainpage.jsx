ac6b26068464ccaea0444c4780edd57be605e077
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2025-02-15 19:56:19 +0900

    폰트 스타일 수정

--- src/pages/Main/mainpage.jsx
+++ src/pages/Main/mainpage.jsx
@@ -180,16 +180,12 @@ function MainPage() {
                 <div className="OgleMent">
                   <span className="phar">
                     우리가 사용하는 평범하고도 대단한 많은 것들은 처음엔 <br />
-                  </span>
-                  <span className="phar">
                     누군가의 상상에 의해 만들어졌습니다.
                     <br />
                   </span>
-                  <span className="phar">
+                  <span className="phar-acc">
                     세상을 구성하는 상상들에는 특별한 힘이 있습니다.
                     <br />
-                  </span>
-                  <span className="phar">
                     임플루드는 상상이 세상을 바꿀 수 있다고 믿습니다.
                     <br />
                   </span>
@@ -199,22 +195,16 @@ function MainPage() {
                   </span>
                   <span className="phar">
                     상상으로 세상을 바꾸기 위해서는
-                    <br />
-                  </span>
-                  <span className="phar">
-                    상상을 현실로 만들어낼 수 있는 능력이 필요합니다.
+                    <br /> 상상을 현실로 만들어낼 수 있는 능력이 필요합니다.
                     <br />
                   </span>
                   <span className="phar">
                     임플루드는 이를 돕기 위해 존재합니다.
                     <br />
                   </span>
-                  <span className="phar">
+                  <span className="phar-acc">
                     우리의 상상이 세상을 바꿀 수 있도록,
-                    <br />
-                  </span>
-                  <span className="phar">
-                    임플루드는 오늘도 달리고 있습니다.
+                    <br /> 임플루드는 오늘도 달리고 있습니다.
                   </span>
                 </div>
               </div>
