888991238b303eea26f945b8cd10e44ce48d6b0a
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2025-02-15 18:07:53 +0900

    스타일 일부 수정

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import Router from './Router.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './main.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <SpeedInsights/>
     <BrowserRouter>
       <Router />
     </BrowserRouter>
