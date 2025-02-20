a2d488e3ca5c171fa41c7d8c5b09bc53bcb01dc5
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2025-02-20 21:20:18 +0900

    스타일 변경

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import Router from './Router.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './main.css'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,5 +12,6 @@ createRoot(document.getElementById('root')).render(
       <Router />
     </BrowserRouter>
     <SpeedInsights/>
+    <Analytics />
   </StrictMode>,
 )
